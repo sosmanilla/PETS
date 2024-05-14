82c9d5d5ed7f9d8f2e04355667f4af58c4f533b1
Rolando0210 <rolanrdcv85@gmail.com>
2024-05-14 14:30:53 -0400

    Implementacion de la url del google sheets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const AS_URL_BASE =
-  "";
+  "https://script.google.com/macros/s/AKfycbwJenDnEhc4ZqvXH1k2RgMYrfWhNVEAnVs8V6iY8vXkVPxkDpgmQ1LTN_SboqPR8UAN/exec";
 
 var url = document.location.href;
 var k = url.substr(url.lastIndexOf("k=") + 2);
